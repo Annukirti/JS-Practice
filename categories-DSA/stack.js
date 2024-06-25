76d04a07f5d30bb638b73d202ae7b3c8a3914a76
Annukirti <annukirti2000@gmail.com>
2024-06-25 13:12:05 +0530

    fix: Added Greater Element 1 using stack

--- categories-DSA/stack.js
+++ categories-DSA/stack.js
@@ -124,6 +124,7 @@ function isEmpty(stack) {
 function nextLargerElement(arr, n) {
   let stack = [];
   let result = new Array(n);
+  //   const cache = {}
   for (let i = n - 1; i >= 0; i--) {
     while (stack.length && arr[i] >= stack[stack.length - 1]) {
       stack.pop();
@@ -135,6 +136,47 @@ function nextLargerElement(arr, n) {
   }
   return result;
 }
-console.log(nextLargerElement([6, 8, 0, 1, 3], 5));
-console.log(nextLargerElement([1, 3, 4, 2], 4));
-console.log(nextLargerElement([1, 3, 2, 4], 4));
+// console.log(nextLargerElement([6, 8, 0, 1, 3], 5)); //[ 8, -1, 1, 3, -1 ]
+// console.log(nextLargerElement([1, 3, 4, 2], 4)); //[ 3, 4, -1, -1 ]
+// console.log(nextLargerElement([1, 3, 2, 4], 4)); //[ 3, 4, 4, -1 ]
+
+// ===================================================QUESTION 4=========================================================
+
+// Next Greater Element 1
+// Example 1:
+
+// Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+// Output: [-1,3,-1]
+// Explanation: The next greater element for each value of nums1 is as follows:
+// - 4 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+// - 1 is underlined in nums2 = [1,3,4,2]. The next greater element is 3.
+// - 2 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+// Example 2:
+
+// Input: nums1 = [2,4], nums2 = [1,2,3,4]
+// Output: [3,-1]
+// Explanation: The next greater element for each value of nums1 is as follows:
+// - 2 is underlined in nums2 = [1,2,3,4]. The next greater element is 3.
+// - 4 is underlined in nums2 = [1,2,3,4]. There is no next greater element, so the answer is -1.
+
+function nextGreatestElement(nums1, nums2) {
+  let stack = [];
+  let result = new Array(nums1.length);
+  for (let i = nums2.length - 1; i >= 0; i--) {
+    while (stack.length && nums2[i] >= stack[stack.length - 1]) {
+      stack.pop();
+    }
+    if (nums1.includes(nums2[i])) {
+      let index = nums1.indexOf(nums2[i]);
+      if (stack.length) {
+        result[index] = stack[stack.length - 1];
+      } else {
+        result[index] = -1;
+      }
+    }
+    stack.push(nums2[i]);
+  }
+  return result;
+}
+console.log(nextGreatestElement([4, 1, 2], [1, 3, 4, 2])); // [ -1, 3, -1 ]
+console.log(nextGreatestElement([2, 4], [1, 2, 3, 4])); // [ 3, -1 ]
