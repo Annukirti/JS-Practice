66f07734a00edfad6d49d057ba65b3b4e320aa45
Annukirti <annukirti2000@gmail.com>
2024-07-09 12:14:56 +0530

    fix: Linked List questions

--- categories-DSA/linked-list.js
+++ categories-DSA/linked-list.js
@@ -166,3 +166,209 @@ console.log("printList", list.printList());
 console.log("find element index", list.indexOf(10));
 
 console.log("printList", list.printList());
+
+// ================================= Remove Nth Node From End of List ======================================================
+
+/**
+ * Given the head of a linked list, remove the nth node from the end of the list and return its head.
+Example 1:
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+
+Example 2:
+Input: head = [1], n = 1
+Output: []
+
+Example 3:
+Input: head = [1,2], n = 1
+Output: [1]
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function (head, n) {
+  let slow = head;
+  let fast = head;
+
+  for (let i = 0; i < n; i++) {
+    fast = fast.next;
+  }
+
+  if (!fast) {
+    return head.next;
+  }
+
+  while (fast.next) {
+    fast = fast.next;
+    slow = slow.next;
+  }
+  slow.next = slow.next.next;
+  return head;
+};
+
+// ======================================== Reverse Linked List ======================================================
+
+/**Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Example 2:
+Input: head = [1,2]
+Output: [2,1]
+
+Example 3:
+Input: head = []
+Output: []
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let prev = null;
+  let current = head;
+
+  while (current) {
+    const node = current.next;
+    current.next = prev;
+    prev = current;
+    current = node;
+  }
+  return prev;
+};
+
+// ======================================== 234. Palindrome Linked List ======================================================
+
+/**
+ * Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+
+Example 1:
+Input: head = [1,2,2,1]
+Output: true
+
+Example 2:
+Input: head = [1,2]
+Output: false
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function (head) {
+  let current = head;
+  let string1 = "",
+    string2 = "";
+
+  while (current) {
+    string1 = `${string1}${current.val}`;
+    string2 = `${current.val}${string2}`;
+    current = current.next;
+  }
+  return string1 === string2;
+};
+
+// ======================================== 237. Delete a given node from LinkedList ======================================================
+
+/**
+ * Example 1:
+Input: head = [4,5,1,9], node = 5
+Output: [4,1,9]
+Explanation: You are given the second node with value 5, the linked list should become 4 -> 1 -> 9 after calling your function.
+
+Example 2:
+Input: head = [4,5,1,9], node = 1
+Output: [4,5,9]
+Explanation: You are given the third node with value 1, the linked list should become 4 -> 5 -> 9 after calling your function.
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function (node) {
+  let current = node;
+  current.val = current.next.val;
+  current.next = current.next.next;
+};
+
+// ======================================== 2. Add Two Numbers ======================================================
+
+/**
+ * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+Example 1:
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+
+Example 2:
+Input: l1 = [0], l2 = [0]
+Output: [0]
+
+Example 3:
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+  let dummy = new ListNode(0);
+  let current = dummy;
+  let carry = 0;
+
+  while (l1 != null || l2 != null || carry > 0) {
+    let value1 = l1 ? l1.val : 0;
+    let value2 = l2 ? l2.val : 0;
+
+    let sum = value1 + value2 + carry;
+    carry = Math.floor(sum / 10);
+    sum %= 10;
+
+    current.next = new ListNode(sum);
+    current = current.next;
+
+    if (l1) l1 = l1.next;
+    if (l2) l2 = l2.next;
+  }
+  return dummy.next;
+};
